b098ba107c32b79fa69bb5f5974e84d4b2ac6335
PRANEETH-BADETI <praneethbadeti@gmail.com>
2026-03-25 22:50:50 +0530

    Resolved Secrets Security issue

--- Customer-portal/Backend/config/db.js
+++ Customer-portal/Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://praneethbadeti:Praneeth2006@ac-hlnt7pc-shard-00-00.nd56per.mongodb.net:27017,ac-hlnt7pc-shard-00-01.nd56per.mongodb.net:27017,ac-hlnt7pc-shard-00-02.nd56per.mongodb.net:27017/?ssl=true&replicaSet=atlas-jg2jyp-shard-0&authSource=admin&appName=Cluster0');
+    await mongoose.connect(process.env.DB_LINK);
     console.log("DB Connected");
   } catch (error) {
     console.error("DB Connection Error:", error.message);
